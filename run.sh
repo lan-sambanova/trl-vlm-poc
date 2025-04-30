1da02b80c684f3a3386bd573d88214d79a5b9f27
Lan Gong <lan.gong@sambanovasystems.com>
2025-04-30 16:44:17 -0700

    remove pdb in launch script

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ export HF_DATASET_CACHE=${ACTUAL_CACHE}
 export HF_MODULES_CACHE=${ACTUAL_CACHE}
 export HF_HOME=${ACTUAL_CACHE}
 
-FORCE_TORCHRUN=1 python -m pdb train.py
+FORCE_TORCHRUN=1 python train.py > /import/ml-sc-scratch6/lang/trl-vlm-poc/train_output.log 2>&1
